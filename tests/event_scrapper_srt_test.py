1355df920b23559b7d2643fb693b963546d22aee
Tomasz Wojdat <tpwojdat@gmail.com>
2024-06-27 00:40:37 +0200

    Also move gancio events

--- tests/event_scrapper_srt_test.py
+++ tests/event_scrapper_srt_test.py
@@ -70,45 +70,8 @@ def test_extract_past_event_details(file, expected, caplog):
 @pytest.mark.parametrize(
     ('details', 'expected'),
     (
-        (
-            resources.example_event,
-            {
-                'title': 'Lindy Hop dla początkujacych | intensywne warsztaty',
-                'description': 'Genialny w swej prostocie, bez określonych reguł i sztywnej ramy, pełen szaleństwa i ekspresji, najradośniejszy ze wszystkich tańców na świecie – taki jest właśnie Lindy Hop! 😉 Jest on najpopularniejszym tańcem swingowym i przygode ze swingiem polecamy zacząć własnie od niego.',
-                'place_name': 'Studio Swing Revolution Trójmiasto',
-                'place_address': 'Łąkowa 35/38, Gdańsk',
-                'start_datetime': 1722074400,
-                'multidate': 0,
-                'tags': '["swing"]',
-                'recurrent': {'days': ['2024-07-27T12:00:00']},
-                'image': b'',
-            },
-        ),
-        (
-            resources.example_event_recurring,
-            {
-                'title': 'Sunday Summer Night | CONIEDZIELNA POTAŃCÓWKA',
-                'description': '<p>Wyobraźcie sobie letni, niedzielny wieczór… 🌅 Dzień powoli się kończy, ale jednak czegoś brakuje do pełnego spełnienia. Zaczynasz szukać potańcówki tu i tam, i nic nie ma! Kto w niedzielę robi potańcówki? Kto robi imprezy regularnie, tak by nie musieć się zastanawiać i ich szukać? 🤔</p><p>Otóż… MY! 😃 Kochani, zapraszamy Was na Sunday Summer Night! Co niedzielę przez lipiec i sierpień otwieramy nasze studio o 20:00, organizujemy DJ’kę i bawimy się do 23:00. 🎶 </p><p>Bez socialu nie ma tańca, więc zapraszamy wszystkich Lindy Hopersów, tancerzy Solo Jazz, Boogie i Balboa Maniaków na parkiet! 💃🕺 Chcemy stworzyć kolejną okazję do tańczenia i integracji naszej trójmiejskiej społeczności.</p><p>Jeśli jesteś nowy/nowa, przyjdź i poproś kogoś o pokazanie podstawowych kroków. 👟 Tylko ci, którzy spróbowali dołączyć do nas, wiedzą, jak łatwa i przyjemna to sprawa, a nasza społeczność słynie ze swojej otwartości i przyjazności. 🌟</p><p>Do zobaczenia na parkiecie!</p><p>– Coniedziele 7 lipca – 25 sierpnia, Studio SRT, 20:00-23:00</p>',
-                'place_name': 'Studio Swing Revolution Trójmiasto',
-                'place_address': 'ul. Łąkowa 35/38',
-                'start_datetime': 1720375200,
-                'multidate': 0,
-                'tags': '["swing"]',
-                'recurrent': {
-                    'days': [
-                        '2024-07-07T20:00:00',
-                        '2024-07-14T20:00:00',
-                        '2024-07-21T20:00:00',
-                        '2024-07-28T20:00:00',
-                        '2024-08-04T20:00:00',
-                        '2024-08-11T20:00:00',
-                        '2024-08-18T20:00:00',
-                        '2024-08-25T20:00:00',
-                    ]
-                },
-                'image': b'',
-            },
-        ),
+        (resources.example_event, resources.example_event_gancio),
+        (resources.example_event_recurring, resources.example_event_recurring_gancio),
     ),
 )
 def test_prepare_gancio_event(details, expected):
