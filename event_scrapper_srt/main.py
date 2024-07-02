5afd1d5544e044912d643f6871490ee194f3da5d
Tomasz Wojdat <tpwojdat@gmail.com>
2024-07-03 00:33:05 +0200

    Use module-level imports in `main.py`

--- event_scrapper_srt/main.py
+++ event_scrapper_srt/main.py
@@ -7,27 +7,26 @@ from dataclasses import asdict
 from datetime import datetime
 from pprint import pformat
 
+from event_scrapper_srt import gancio
+from event_scrapper_srt import scrapper
+from event_scrapper_srt import sitemap
+from event_scrapper_srt import util
 from event_scrapper_srt.event import Event
-from event_scrapper_srt.gancio import add_event_requests
-from event_scrapper_srt.gancio import get_gancio_events
-from event_scrapper_srt.scrapper import get_events
-from event_scrapper_srt.sitemap import get_elements
-from event_scrapper_srt.util import get_url_content
 
 SITEMAP_URL = 'https://swingrevolution.pl/events-sitemap.xml'
 
 
 def main() -> int:
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    xml_content = get_url_content(SITEMAP_URL)
-    sitemap_elems = get_elements(xml_content)
-    events = get_events(sitemap_elems)
+    xml_content = util.get_url_content(SITEMAP_URL)
+    sitemap_elems = sitemap.get_elements(xml_content)
+    events = scrapper.get_events(sitemap_elems)
     dump_events_to_json(events, folder='output')
-    gancio_events = get_gancio_events(events)
+    gancio_events = gancio.get_gancio_events(events)
     # There is a default rate limiting 5 requests per 5 minutes, so we
     # iterate only on a few events
     for event in gancio_events[:5]:
-        response = add_event_requests(event, instance_url='http://127.0.0.1:13120')
+        response = gancio.add_event_requests(event, instance_url='http://127.0.0.1:13120')
         logging.info(f'Event added:\n{pformat(response)}')
         print(''.center(80, '-'))
     return 0
