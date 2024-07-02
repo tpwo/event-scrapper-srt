0b61fbbfd4b4d77c22e5442294712486a62d6ad0
Tomasz Wojdat <tpwojdat@gmail.com>
2024-07-03 01:01:31 +0200

    Add test for scrapper.get_events

--- testing/resources.py
+++ testing/resources.py
@@ -8,7 +8,7 @@ from event_scrapper_srt.event import GancioEvent
 from event_scrapper_srt.event import Occurrence
 
 example_event = Event(
-    url='https://example.com/',
+    url='testing/example-event.html',
     title='Lindy Hop dla początkujacych | intensywne warsztaty',
     description='<p>Daj się zarazić swingowym bakcylem podczas intensywnych warsztatów od podstaw! Nie musisz nic umieć (większość z nas tak właśnie zaczynała), a jeśli plączą Ci się nogi – wspólnie je rozplączemy. 🙂 Udowodnimy Ci, że taniec może być prosty i przyjemny, a to wszystko w doborowym towarzystwie pozytywnie zakręconych ludzi i przy dźwiękach porywającego do tańca swinga.</p><p>🔸 ZAPISY 🔸<br/> · Zajęcia odbędą się w sobotę 27 lipca (3h, od 12:00-15:00).<br/> · Na zajęciach zmieniamy się w parach.<br/> · Nie potrzebujesz pary do wzięcia udziału w zajęciach. Przy zapisach dbamy o odpowiednie proporcje w grupie.<br/> · Każda osoba musi wypełnić osobny formularz (nawet gdy zapisujesz się w parze).<br/> ❗ Ilość miejsc na zajęciach jest ograniczona.</p>',
     place_name='Studio Swing Revolution Trójmiasto',
@@ -32,7 +32,7 @@ example_event_gancio = [
         description='<p>Daj się zarazić swingowym bakcylem podczas intensywnych warsztatów od podstaw! Nie musisz nic umieć (większość z nas tak właśnie zaczynała), a jeśli plączą Ci się nogi – wspólnie je rozplączemy. 🙂 Udowodnimy Ci, że taniec może być prosty i przyjemny, a to wszystko w doborowym towarzystwie pozytywnie zakręconych ludzi i przy dźwiękach porywającego do tańca swinga.</p><p>🔸 ZAPISY 🔸<br/> · Zajęcia odbędą się w sobotę 27 lipca (3h, od 12:00-15:00).<br/> · Na zajęciach zmieniamy się w parach.<br/> · Nie potrzebujesz pary do wzięcia udziału w zajęciach. Przy zapisach dbamy o odpowiednie proporcje w grupie.<br/> · Każda osoba musi wypełnić osobny formularz (nawet gdy zapisujesz się w parze).<br/> ❗ Ilość miejsc na zajęciach jest ograniczona.</p>',
         place_name='Studio Swing Revolution Trójmiasto',
         place_address='Łąkowa 35/38, Gdańsk',
-        online_locations=['https://example.com/'],
+        online_locations=['testing/example-event.html'],
         start_datetime=1722074400,
         end_datetime=1722085200,
         multidate=1,
@@ -42,7 +42,7 @@ example_event_gancio = [
 ]
 
 example_event_recurring = Event(
-    url='https://example.com/',
+    url='testing/example-event-recurring.html',
     title='Sunday Summer Night | CONIEDZIELNA POTAŃCÓWKA',
     description='<p>Wyobraźcie sobie letni, niedzielny wieczór… 🌅 Dzień powoli się kończy, ale jednak czegoś brakuje do pełnego spełnienia. Zaczynasz szukać potańcówki tu i tam, i nic nie ma! Kto w niedzielę robi potańcówki? Kto robi imprezy regularnie, tak by nie musieć się zastanawiać i ich szukać? 🤔</p><p>Otóż… MY! 😃 Kochani, zapraszamy Was na Sunday Summer Night! Co niedzielę przez lipiec i sierpień otwieramy nasze studio o 20:00, organizujemy DJ’kę i bawimy się do 23:00. 🎶 </p><p>Bez socialu nie ma tańca, więc zapraszamy wszystkich Lindy Hopersów, tancerzy Solo Jazz, Boogie i Balboa Maniaków na parkiet! 💃🕺 Chcemy stworzyć kolejną okazję do tańczenia i integracji naszej trójmiejskiej społeczności.</p><p>Jeśli jesteś nowy/nowa, przyjdź i poproś kogoś o pokazanie podstawowych kroków. 👟 Tylko ci, którzy spróbowali dołączyć do nas, wiedzą, jak łatwa i przyjemna to sprawa, a nasza społeczność słynie ze swojej otwartości i przyjazności. 🌟</p><p>Do zobaczenia na parkiecie!</p><p>– Coniedziele 7 lipca – 25 sierpnia, Studio SRT, 20:00-23:00</p>',
     place_name='Studio Swing Revolution Trójmiasto',
@@ -73,7 +73,7 @@ example_event_recurring_gancio = [
         description='<p>Wyobraźcie sobie letni, niedzielny wieczór… 🌅 Dzień powoli się kończy, ale jednak czegoś brakuje do pełnego spełnienia. Zaczynasz szukać potańcówki tu i tam, i nic nie ma! Kto w niedzielę robi potańcówki? Kto robi imprezy regularnie, tak by nie musieć się zastanawiać i ich szukać? 🤔</p><p>Otóż… MY! 😃 Kochani, zapraszamy Was na Sunday Summer Night! Co niedzielę przez lipiec i sierpień otwieramy nasze studio o 20:00, organizujemy DJ’kę i bawimy się do 23:00. 🎶 </p><p>Bez socialu nie ma tańca, więc zapraszamy wszystkich Lindy Hopersów, tancerzy Solo Jazz, Boogie i Balboa Maniaków na parkiet! 💃🕺 Chcemy stworzyć kolejną okazję do tańczenia i integracji naszej trójmiejskiej społeczności.</p><p>Jeśli jesteś nowy/nowa, przyjdź i poproś kogoś o pokazanie podstawowych kroków. 👟 Tylko ci, którzy spróbowali dołączyć do nas, wiedzą, jak łatwa i przyjemna to sprawa, a nasza społeczność słynie ze swojej otwartości i przyjazności. 🌟</p><p>Do zobaczenia na parkiecie!</p><p>– Coniedziele 7 lipca – 25 sierpnia, Studio SRT, 20:00-23:00</p>',
         place_name='Studio Swing Revolution Trójmiasto',
         place_address='ul. Łąkowa 35/38',
-        online_locations=['https://example.com/'],
+        online_locations=['testing/example-event-recurring.html'],
         start_datetime=1720375200,
         end_datetime=1720386000,
         multidate=1,
@@ -85,7 +85,7 @@ example_event_recurring_gancio = [
         description='<p>Wyobraźcie sobie letni, niedzielny wieczór… 🌅 Dzień powoli się kończy, ale jednak czegoś brakuje do pełnego spełnienia. Zaczynasz szukać potańcówki tu i tam, i nic nie ma! Kto w niedzielę robi potańcówki? Kto robi imprezy regularnie, tak by nie musieć się zastanawiać i ich szukać? 🤔</p><p>Otóż… MY! 😃 Kochani, zapraszamy Was na Sunday Summer Night! Co niedzielę przez lipiec i sierpień otwieramy nasze studio o 20:00, organizujemy DJ’kę i bawimy się do 23:00. 🎶 </p><p>Bez socialu nie ma tańca, więc zapraszamy wszystkich Lindy Hopersów, tancerzy Solo Jazz, Boogie i Balboa Maniaków na parkiet! 💃🕺 Chcemy stworzyć kolejną okazję do tańczenia i integracji naszej trójmiejskiej społeczności.</p><p>Jeśli jesteś nowy/nowa, przyjdź i poproś kogoś o pokazanie podstawowych kroków. 👟 Tylko ci, którzy spróbowali dołączyć do nas, wiedzą, jak łatwa i przyjemna to sprawa, a nasza społeczność słynie ze swojej otwartości i przyjazności. 🌟</p><p>Do zobaczenia na parkiecie!</p><p>– Coniedziele 7 lipca – 25 sierpnia, Studio SRT, 20:00-23:00</p>',
         place_name='Studio Swing Revolution Trójmiasto',
         place_address='ul. Łąkowa 35/38',
-        online_locations=['https://example.com/'],
+        online_locations=['testing/example-event-recurring.html'],
         start_datetime=1720980000,
         end_datetime=1720990800,
         multidate=1,
@@ -96,7 +96,7 @@ example_event_recurring_gancio = [
 
 
 example_event_past = Event(
-    url='https://example.com/',
+    url='testing/example-event-past.html',
     title='Swingowa potańcówka nad Motławą',
     description='<p>Zapraszamy serdecznie na kolejną z serii potańcówek swingowych nad Motławą.</p><p>Jest to wspólna inicjatywa 3 szkół tańców swingowych z Trójmiasta (Harlem Beats, Swing Revolution Trójmiasto, Shag College) we współpracy z ORIA MAGIC HOUSE w sercu Gdańska nad samą Motławą, której celem jest zintegrowanie środowiska tancerzy swingowych w Trójmieście. Chcemy również podczas krótkich lekcji pokazowych prezentować tańce swingowe szerszej publiczności, dlatego wpadnij na imprezkę w klimacie swingowym i zabierz ze sobą znajomych</p><p>ORIA MAGIC HOUSE nad Motławą to piękne miejsce, które jest zarówno restauracją, jak i teatrem i galerią sztuki, organizatorem wielu koncertów i imprez kulturalnych. Można tu coś dobrego zjeść i napić się zimowej herbaty</p>',
     place_name='Oria Magic House',
--- tests/scrapper_test.py
+++ tests/scrapper_test.py
@@ -10,6 +10,24 @@ from testing import resources
 
 
 @pytest.mark.parametrize(
+    ('url', 'expected'),
+    (
+        ('example-event.html', [resources.example_event]),
+        ('example-event-recurring.html', [resources.example_event_recurring]),
+    ),
+)
+def test_get_events(url, expected):
+    actual = scrapper.get_events([f'testing/{url}'], html_getter=get_html_fake)
+    assert actual[0] == expected[0]
+    assert len(actual) == len(expected) == 1
+
+
+def get_html_fake(path: str) -> bytes:
+    """Fake HTML getter. Instead of HTML it returns content of a file."""
+    return pathlib.Path(path).read_bytes()
+
+
+@pytest.mark.parametrize(
     ('file', 'expected'),
     (
         ('example-event.html', resources.example_event),
@@ -18,7 +36,7 @@ from testing import resources
 )
 def test_extract_event_details(file, expected):
     html_content = pathlib.Path(f'testing/{file}').read_text()
-    actual = scrapper.extract_event_details(html_content=html_content, url='https://example.com/')
+    actual = scrapper.extract_event_details(html_content=html_content, url=f'testing/{file}')
     assert actual == expected
 
 
@@ -29,6 +47,6 @@ def test_extract_event_details(file, expected):
 def test_extract_past_event_details(file, expected, caplog):
     caplog.set_level(logging.INFO)
     html_content = pathlib.Path(f'testing/{file}').read_text()
-    actual = scrapper.extract_event_details(html_content=html_content, url='https://example.com/')
+    actual = scrapper.extract_event_details(html_content=html_content, url=f'testing/{file}')
     assert actual == expected
     assert 'Past event found: no date and time information' in caplog.text
