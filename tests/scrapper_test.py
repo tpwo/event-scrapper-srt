5c3b09ed4b7bb2c5eb7aafcc5048f896f9f3daf7
Tomasz Wojdat <tpwojdat@gmail.com>
2024-07-03 01:05:38 +0200

    Add test for scrapper.get_events also for a past event

--- tests/scrapper_test.py
+++ tests/scrapper_test.py
@@ -22,6 +22,21 @@ def test_get_events(url, expected):
     assert len(actual) == len(expected) == 1
 
 
+@pytest.mark.parametrize(
+    ('url', 'expected'),
+    (('example-event-past.html', [resources.example_event_past]),),
+)
+def test_get_events_past(url, expected, caplog):
+    caplog.set_level(logging.INFO)
+    actual = scrapper.get_events([f'testing/{url}'], html_getter=get_html_fake)
+    assert actual[0] == expected[0]
+    assert len(actual) == len(expected) == 1
+    assert (
+        'Past event found: no date and time information in `Swingowa potańcówka nad Motławą`'
+        in caplog.text
+    )
+
+
 def get_html_fake(path: str) -> bytes:
     """Fake HTML getter. Instead of HTML it returns content of a file."""
     return pathlib.Path(path).read_bytes()
