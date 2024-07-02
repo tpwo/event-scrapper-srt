065004557ccd599b550c832d04be183334dce159
Tomasz Wojdat <tpwojdat@gmail.com>
2024-07-03 01:06:10 +0200

    Remove tests for extract_event_details
    These are implementation details which don't need unit testing.

--- tests/scrapper_test.py
+++ tests/scrapper_test.py
@@ -40,28 +40,3 @@ def test_get_events_past(url, expected, caplog):
 def get_html_fake(path: str) -> bytes:
     """Fake HTML getter. Instead of HTML it returns content of a file."""
     return pathlib.Path(path).read_bytes()
-
-
-@pytest.mark.parametrize(
-    ('file', 'expected'),
-    (
-        ('example-event.html', resources.example_event),
-        ('example-event-recurring.html', resources.example_event_recurring),
-    ),
-)
-def test_extract_event_details(file, expected):
-    html_content = pathlib.Path(f'testing/{file}').read_text()
-    actual = scrapper.extract_event_details(html_content=html_content, url=f'testing/{file}')
-    assert actual == expected
-
-
-@pytest.mark.parametrize(
-    ('file', 'expected'),
-    (('example-event-past.html', resources.example_event_past),),
-)
-def test_extract_past_event_details(file, expected, caplog):
-    caplog.set_level(logging.INFO)
-    html_content = pathlib.Path(f'testing/{file}').read_text()
-    actual = scrapper.extract_event_details(html_content=html_content, url=f'testing/{file}')
-    assert actual == expected
-    assert 'Past event found: no date and time information' in caplog.text
