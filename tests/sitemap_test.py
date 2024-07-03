0dd5b4d138bfd160b249756c1f312f7b7e2ce165
Tomasz Wojdat <tpwojdat@gmail.com>
2024-07-03 02:01:47 +0200

    Add test for sitemap.get_urls

--- tests/sitemap_test.py
+++ tests/sitemap_test.py
@@ -6,6 +6,20 @@ import freezegun
 
 from event_scrapper_srt import sitemap
 from event_scrapper_srt.sitemap import SitemapElem
+from testing import fakes
+
+
+@freezegun.freeze_time('2023-02-28')
+def test_get_urls():
+    actual = sitemap.get_urls(
+        'testing/example-events-sitemap.xml', content_getter=fakes.content_getter
+    )
+    expected = [
+        'https://swingrevolution.pl/wydarzenia/sunday-summer-night-coniedzielna-potancowka/',
+        'https://swingrevolution.pl/wydarzenia/practice-chill/',
+        'https://swingrevolution.pl/wydarzenia/w-rytmie-swinga-potancowka/',
+    ]
+    assert actual == expected
 
 
 @freezegun.freeze_time('2023-02-28')
