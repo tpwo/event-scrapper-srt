d5177095cbfcef9d50efdad22cd4a5a3093683d6
Tomasz Wojdat <tpwojdat@gmail.com>
2024-07-10 21:50:31 +0200

    No longer offer to publish events to Gancio from main
    Now, the `main` function focuses only to saving scrapped output to
    NDJSON file. Remove related CLI args which are no longer used.

--- event_scrapper_srt/main.py
+++ event_scrapper_srt/main.py
@@ -6,7 +6,6 @@ import logging
 from dataclasses import asdict
 from datetime import datetime
 from pathlib import Path
-from pprint import pformat
 
 from event_scrapper_srt import gancio
 from event_scrapper_srt import scrapper
@@ -14,7 +13,6 @@ from event_scrapper_srt import sitemap
 from event_scrapper_srt.event import GancioEvent
 
 SITEMAP_URL = 'https://swingrevolution.pl/events-sitemap.xml'
-GANCIO_URL = 'http://127.0.0.1:13120'
 
 
 def main(argv: list[str] | None = None) -> int:
@@ -26,42 +24,17 @@ def main(argv: list[str] | None = None) -> int:
         help='events are scrapped from here (default: %(default)s)',
     )
     parser.add_argument(
-        '--gancio-url', default=GANCIO_URL, help='events are published there (default: %(default)s)'
-    )
-    parser.add_argument(
         '--output-path',
         default=f'output/{datetime.now().isoformat()}.json',
         help='JSON with scrapped events is saved there (default: %(default)s)',
-    )
-    parser.add_argument(
-        '--max-events-publish',
-        type=int,
-        default=5,
-        help='publish maximum that many events to gancio (default: %(default)s)',
-    )
-    parser.add_argument(
-        '--no-confirm',
-        action='store_true',
-        help='do not ask for confirmation before publishing events',
     )
     args = parser.parse_args(argv)
 
     events = scrapper.get_events(sitemap.get_urls(args.sitemap_url))
     gancio_events = gancio.create_events(events)
+
     dump_events_to_json(gancio_events, output_path=args.output_path)
-    logging.info(f'Prepared {len(gancio_events)} Gancio events for publishing')
 
-    confirm = not args.no_confirm
-    if confirm:
-        if input(f'Continue with publishing events to `{args.gancio_url}` (y/N)? ') != 'y':
-            raise SystemExit('Aborting')
-
-    # There is a default rate limiting 5 requests per 5 minutes, so we
-    # iterate only on a few events
-    for event in gancio_events[: args.max_events_publish]:
-        response = gancio.add_event(event, instance_url=args.gancio_url)
-        logging.info(f'Event added:\n{pformat(response)}')
-        print(''.center(80, '-'))
     return 0
 
 
@@ -77,7 +50,7 @@ def dump_events_to_json(events: list[GancioEvent], output_path: str) -> None:
         for event in events:
             json.dump(asdict(event), file, indent=None, ensure_ascii=False, default=str)
             file.write('\n')
-    logging.info(f'Saved {len(events)} to `{path.absolute()}`')
+    logging.info(f'Saved {len(events)} events to `{path.absolute()}`')
 
 
 if __name__ == '__main__':
