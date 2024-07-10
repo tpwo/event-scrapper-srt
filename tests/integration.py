ba8a0c2a2a3841d3246e6ccf9b6c8925d987f372
Tomasz Wojdat <tpwojdat@gmail.com>
2024-07-10 19:58:48 +0200

    Compare almost everything in integration test

--- tests/integration.py
+++ tests/integration.py
@@ -40,7 +40,7 @@ def test_add_event(gancio_instance):
             'likes': [],
             'boost': [],
             'online_locations': [],
-            'id': 5,
+            'id': 1,
             'title': 'Lindy Hop dla początkujacych | intensywne warsztaty',
             'description': '<p>Daj się zarazić swingowym bakcylem podczas intensywnych warsztatów od podstaw! Nie musisz nic umieć (większość z nas tak właśnie zaczynała), a jeśli plączą Ci się nogi – wspólnie je rozplączemy. 🙂 Udowodnimy Ci, że taniec może być prosty i przyjemny, a to wszystko w doborowym towarzystwie pozytywnie zakręconych ludzi i przy dźwiękach porywającego do tańca swinga.</p><p>🔸 ZAPISY 🔸<br> · Zajęcia odbędą się w sobotę 27 lipca (3h, od 12:00-15:00).<br> · Na zajęciach zmieniamy się w parach.<br> · Nie potrzebujesz pary do wzięcia udziału w zajęciach. Przy zapisach dbamy o odpowiednie proporcje w grupie.<br> · Każda osoba musi wypełnić osobny formularz (nawet gdy zapisujesz się w parze).<br> ❗ Ilość miejsc na zajęciach jest ograniczona.</p>',
             'multidate': '1',
@@ -49,22 +49,32 @@ def test_add_event(gancio_instance):
             'recurrent': None,
             'is_visible': False,
             'media': [],
-            'updatedAt': '2024-06-30T20:23:57.641Z',
-            'createdAt': '2024-06-30T20:23:57.627Z',
-            'slug': 'lindy-hop-dla-poczatkujacych-or-intensywne-warsztaty-4',
+            'updatedAt': '<NOT COMPARED>',
+            'createdAt': '<NOT COMPARED>',
+            'slug': 'lindy-hop-dla-poczatkujacych-or-intensywne-warsztaty',
             'placeId': 1,
             'tags': [],
             'place': {
                 'id': 1,
                 'name': 'Studio Swing Revolution Trójmiasto',
-                'address': 'ul. Łąkowa 35/38',
+                'address': 'Łąkowa 35/38, Gdańsk',
                 'latitude': None,
                 'longitude': None,
-                'createdAt': '2024-06-30T16:53:41.996Z',
-                'updatedAt': '2024-06-30T16:53:41.996Z',
+                'createdAt': '<NOT COMPARED>',
+                'updatedAt': '<NOT COMPARED>',
             },
         }
         actual = gancio.add_event_requests(event, instance_url='http://127.0.0.1:13121')
-        # Output is dynamic so we only compare selected attributes
-        for key in ('title', 'description', 'start_datetime', 'end_datetime'):
+
+        assert isinstance(actual['place'], dict)
+        assert isinstance(expected['place'], dict)
+
+        # We compare everything apart from dynamic fields
+        compared_keys = set(expected.keys()) - {'updatedAt', 'createdAt', 'place'}
+        compared_place_keys = set(expected['place'].keys()) - {'createdAt', 'updatedAt'}
+
+        for key in compared_keys:
             assert actual[key] == expected[key]
+
+        for place_key in compared_place_keys:
+            assert actual['place'][place_key] == expected['place'][place_key]
